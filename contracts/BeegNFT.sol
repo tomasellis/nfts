08970e49c068d203f78092d16cd264b0ef2c7c46
Tomás Ellis <tomasitoellis@gmail.com>
2021-10-20 23:52:55 -0300

    feat: pseudo randomnes, 3 word selection and dynamic svg to contract

--- contracts/BeegNFT.sol
+++ contracts/BeegNFT.sol
@@ -14,9 +14,44 @@ contract BeegNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
+  // Word holders
+  string[] firstWords = ["Big", "Beeg", "BiteSized", "Small", "Huge", "Yuge"];
+
+  string[] secondWords = ["Fiery", "Watery", "Earthy", "Voidy", "Luxy", "Aery"];
+
+  string[] thirdWords = ["Sword", "Shield", "Hammer", "Bow", "Spear", "Gun"];
+
+  //SVG holder
+  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+
   // We need to pass the name of our NFTs token and it's symbol.
   constructor() ERC721 ("SquareNFT", "SQUARE") {
     console.log("This is my NFT contract. Woah!");
+  }
+
+  // Functions to pick random words
+  function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+    rand = rand % firstWords.length;
+    return firstWords[rand];
+  }  
+  
+  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+    rand = rand % secondWords.length;
+    return secondWords[rand];
+  }
+
+  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+    rand = rand % thirdWords.length;
+    return thirdWords[rand];
+  }
+
+  // Pseudo on chain random
+  function random(string memory input) internal pure returns (uint256) {
+      return uint256(keccak256(abi.encodePacked(input)));
   }
 
   // A function our user will hit to get their NFT.
@@ -24,6 +59,18 @@ contract BeegNFT is ERC721URIStorage {
      // Get the current tokenId, this starts at 0.
     uint256 newItemId = _tokenIds.current();
 
+    // Get words
+    string memory first = pickRandomFirstWord(newItemId);
+    string memory second = pickRandomSecondWord(newItemId);
+    string memory third = pickRandomThirdWord(newItemId);
+
+    // Close the svg
+    string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+    
+    console.log("\n--------------------");  
+    console.log(finalSvg);
+    console.log("--------------------\n");
+    
      // Actually mint the NFT to the sender using msg.sender.
     _safeMint(msg.sender, newItemId);
 
@@ -36,6 +83,8 @@ contract BeegNFT is ERC721URIStorage {
     // Increment the counter for when the next NFT is minted.
     _tokenIds.increment();
   }
+
+  function pickRandomFirstWord
 }
 
 
